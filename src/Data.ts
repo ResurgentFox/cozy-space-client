168bc06b7462c06698300f3b7d2b4c6365919989
Ann <annkulakova96@gmail.com>
2021-10-03 20:09:02 +0300

    added ws-heartbeat support on client side

--- src/Data.ts
+++ src/Data.ts
@@ -1,14 +1,24 @@
 import { FullPost, Post } from './types'
+import { setWsHeartbeat } from 'ws-heartbeat/client'
 
 const serverAddress = process.env.REACT_APP_URL || '127.0.0.1:3001'
+
 export class Posts {
   private ws: WebSocket
   private handleNewPost?: (post: FullPost) => void 
   
   constructor() {
     this.ws = new WebSocket(`wss://${serverAddress}`)
+    setWsHeartbeat(this.ws, '{"kind":"ping"}', {
+      pingTimeout: 60000,
+      pingInterval: 25000, 
+    })
     this.ws.onmessage = event => { 
-      this.handleNewPost?.(JSON.parse(event.data))
+      const { data } = event
+      const parsed = JSON.parse(data)
+      if (parsed.name && parsed.text) {
+        this.handleNewPost?.(parsed)
+      } 
     }
   }
 
